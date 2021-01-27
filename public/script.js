7f872fbc25412620a3c9d1da4e1610df58de2561
Rafael Evangelista <rafael.evans@Live.com>
2021-01-27 00:30:36 -0300

    Setup project, install and config babel

---
+++ public/script.js
@@ -0,0 +1,6 @@
+/**
+ * babel: Converter (transpilar) código do React p/ um código que o browser entenda
+ * webpack: P/ cada tipo de arquivo (.js, .css, .png) converter o código de uma maneira diferente
+ * 
+ * loaders: babel=loader, css=loadar, image=loader
+ */
